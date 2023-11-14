62d516dfacb94fd8275a921c5f65639132bf3992
huntj88 <huntj88@gmail.com>
2023-11-13 19:52:36 -0600

    send weight sensor values over bluetooth in json format

--- esp32/digitalHangBoard/digitalHangBoard.ino
+++ esp32/digitalHangBoard/digitalHangBoard.ino
@@ -1,12 +1,25 @@
 #include <BluetoothSerial.h>
 #include "HX711.h"
+#include <ArduinoJson.h>
 
 // HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 16; // rx pin on my esp32
-const int LOADCELL_SCK_PIN = 17; // tx pin on my esp32
+const int LOADCELL_DOUT_PIN_0 = 16;  // rx pin on my esp32
+const int LOADCELL_SCK_PIN_0 = 17;   // tx pin on my esp32
+
+const int LOADCELL_DOUT_PIN_1 = 18;
+const int LOADCELL_SCK_PIN_1 = 19;
+
+const int LOADCELL_DOUT_PIN_2 = 20;
+const int LOADCELL_SCK_PIN_2 = 21;
+
+const int LOADCELL_DOUT_PIN_3 = 22;
+const int LOADCELL_SCK_PIN_3 = 23;
 
 BluetoothSerial SerialBT;
-HX711 scale;
+HX711 scale0;  // far left
+HX711 scale1;
+HX711 scale2;
+HX711 scale3;  // far right
 
 /* Check if Bluetooth configurations are enabled in the SDK */
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -15,25 +28,39 @@ HX711 scale;
 
 void setup() {
   Serial.begin(115200);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale0.begin(LOADCELL_DOUT_PIN_0, LOADCELL_SCK_PIN_0);
+  scale1.begin(LOADCELL_DOUT_PIN_1, LOADCELL_SCK_PIN_1);
+  scale2.begin(LOADCELL_DOUT_PIN_2, LOADCELL_SCK_PIN_2);
+  scale3.begin(LOADCELL_DOUT_PIN_3, LOADCELL_SCK_PIN_3);
   SerialBT.begin();
   Serial.println("Bluetooth Started! Ready to pair...");
 }
 
 void loop() {
+  StaticJsonDocument<512> sensorJson;
+  JsonArray scalesJson = sensorJson.createNestedArray("scales");
 
-  if (scale.is_ready()) {
-    long reading = scale.read();
-    if (SerialBT.available()) {
-      SerialBT.println(reading);
-    } else {
-      Serial.println("Bluetooth not available");
-    }
-    Serial.print("HX711 reading: ");
-    Serial.println(reading);
+  scalesJson.add(readScale(scale0));
+  scalesJson.add(readScale(scale1));
+  scalesJson.add(readScale(scale2));
+  scalesJson.add(readScale(scale3));
+
+  serializeJson(sensorJson, Serial);
+  Serial.println();
+  if (SerialBT.available()) {
+    serializeJson(sensorJson, SerialBT);
+    SerialBT.println();
   } else {
-    Serial.println("HX711 not found.");
+    Serial.println("Bluetooth not available");
   }
 
   delay(1000);
+}
+
+int readScale(HX711 scale) {
+  if (scale.is_ready()) {
+    return scale.read();
+  } else {
+    return -1;
+  }
 }
