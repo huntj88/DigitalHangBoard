17bce2d3380d8867d0663e77c6c95a2c533452e0
huntj88 <huntj88@gmail.com>
2023-11-13 19:56:50 -0600

    check if object exists

--- web/src/app/jsBridge.js
+++ web/src/app/jsBridge.js
@@ -1,9 +1,11 @@
 
 export function setupPostMessaging() {
-    global.myObject.onmessage = function(event) {
-        // prints "Got it!" when we receive the app's response.
-        console.log(event.data);
+    if (global.myObject) {
+        global.myObject.onmessage = function (event) {
+            // prints "Got it!" when we receive the app's response.
+            console.log(event.data);
+        }
+        global.myObject.postMessage("I'm ready!");
+        console.log("setup post messaging");
     }
-    global.myObject.postMessage("I'm ready!");
-    console.log("setup post messaging");
 }
