b1b0dd063bfe0c94d15f7965fb865c9238337dc8
huntj88 <huntj88@gmail.com>
2023-11-13 20:50:31 -0600

    setup post messaging between android and webview

--- android/app/src/main/java/me/jameshunt/digitialhangboard/MainActivity.kt
+++ android/app/src/main/java/me/jameshunt/digitialhangboard/MainActivity.kt
@@ -1,14 +1,30 @@
 package me.jameshunt.digitialhangboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
+import androidx.webkit.WebViewCompat
+import androidx.webkit.WebViewCompat.WebMessageListener
+import androidx.webkit.WebViewFeature
+
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val myWebView = WebView(this)
-        setContentView(myWebView)
-        myWebView.loadUrl("https://digital-hang-board.vercel.app/")
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        val myListener =
+            WebMessageListener { view, message, sourceOrigin, isMainFrame, replyProxy -> // do something about view, message, sourceOrigin and isMainFrame.
+                if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_MESSAGE_LISTENER)) {
+                    replyProxy.postMessage("Got it!")
+                }
+            }
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_MESSAGE_LISTENER)) {
+            WebViewCompat.addWebMessageListener(webView, "myObject", setOf("https://digital-hang-board.vercel.app"), myListener)
+        }
+        setContentView(webView)
+        webView.loadUrl("https://digital-hang-board.vercel.app/")
     }
 }
